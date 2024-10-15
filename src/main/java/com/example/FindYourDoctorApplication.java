d9977bdafafa104e24126cc8afe74bd50b4e81fc
Zbigniew Stasienko <zbyszekstasienko@gmail.com>
2024-10-15 16:46:18 +0200

    change name of main class

--- src/main/java/com/example/FindYourDoctorApplication.java
+++ src/main/java/com/example/FindYourDoctorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FindTourDoctorApplication {
+public class FindYourDoctorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FindTourDoctorApplication.class, args);
+        SpringApplication.run(FindYourDoctorApplication.class, args);
     }
 
 }
