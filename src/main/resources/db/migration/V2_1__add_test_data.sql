c4dc9b2a9c7f9a4ad98e73c24496c9d457e15d97
Zbigniew Stasienko <zbyszekstasienko@gmail.com>
2024-11-19 16:58:00 +0100

    fix main page and make migrations more realistic

--- src/main/resources/db/migration/V2_1__add_test_data.sql
+++ src/main/resources/db/migration/V2_1__add_test_data.sql
@@ -1,9 +1,9 @@
 INSERT INTO doctor (id, picture_id, login, password, name, description) VALUES
-('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000001', 'smith', 'pass', 'name1', 'cardiologist'),
-('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000002', 'jones', 'pass', 'name2', 'surgeon'),
-('00000000-0000-0000-0000-000000000003', '00000000-0000-0000-0000-000000000003', 'brown', 'pass', 'name3', 'dentist');
+('00000000-0000-0000-0000-000000000001', '00000000-0000-0000-0000-000000000001', 'smith', 'pass', 'Dr. Emily Smith - Pediatrician', 'Known for her compassionate approach and expertise in childhood development.'),
+('00000000-0000-0000-0000-000000000002', '00000000-0000-0000-0000-000000000003', 'jones', 'pass', 'Dr. Rajesh Jones - Cardiologist','Renowned cardiologist with a focus on a heart disease management.'),
+('00000000-0000-0000-0000-000000000003', '00000000-0000-0000-0000-000000000002', 'brown', 'pass', 'Dr. Sophia Brown - Orthopedic Surgeon', 'Serves as a medical consultant for professional athletes.');
 
 INSERT INTO "user" (id, login, password, name) VALUES
-('00000000-0000-0000-0000-000000000004', 'doe', 'pass', 'name4'),
-('00000000-0000-0000-0000-000000000005', 'jane', 'pass', 'name5'),
-('00000000-0000-0000-0000-000000000006', 'alice', 'pass', 'name6');
+('00000000-0000-0000-0000-000000000004', 'doe', 'pass', 'John Doe123'),
+('00000000-0000-0000-0000-000000000005', 'jane', 'pass', 'Janee'),
+('00000000-0000-0000-0000-000000000006', 'alice', 'pass', 'Alice M.');
